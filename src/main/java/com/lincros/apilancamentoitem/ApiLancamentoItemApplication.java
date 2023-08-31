8a4589c4d5509fff2ae76decc4686abf3623cd22
Eliseu Divino Santos <eliseusantico@gmail.com>
2023-08-30 22:46:16 -0300

    first commit

---
+++ src/main/java/com/lincros/apilancamentoitem/ApiLancamentoItemApplication.java
@@ -0,0 +1,13 @@
+package com.lincros.apilancamentoitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLancamentoItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLancamentoItemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lincros/apilancamentoitem/ApiLancamentoItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lincros.apilancamentoitem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLancamentoItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
