ea74a69fff95bc3bedbfabd86121d8f2203aa1b8
Eliseu Divino Santos <eliseusantico@gmail.com>
2023-08-30 23:11:47 -0300

    configuracao inicial e definicao de arquitetura

--- src/main/java/com/lincros/apilancamentoitem/ApiLancamentoItemApplication.java
+++ src/main/java/com/lincros/apilancamentoitem/ApiLancamentoItemApplication.java
@@ -1,11 +1,23 @@
 package com.lincros.apilancamentoitem;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
+@RestController
+@RequestMapping("/")
 @SpringBootApplication
 public class ApiLancamentoItemApplication {
 
+	@GetMapping
+	public String getHomeTeste() {
+		log.info("Pagina Home");
+		return "Gestão Cotação Facil - API";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ApiLancamentoItemApplication.class, args);
 	}
---
+++ src/main/java/com/lincros/apilancamentoitem/lancamento/domain/entities/Lacamento.java
@@ -0,0 +1,4 @@
+package com.lincros.apilancamentoitem.lancamento.domain.entities;
+
+public class Lacamento {
+}
